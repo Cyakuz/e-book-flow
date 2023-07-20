2c896044a07e2f33d1df35eab2f319a7f5db8e03
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-07-20 03:30:03 +0300

    Panel ve FontAwesome

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import ReactFlow, {
   Background,
   useNodesState,
   useEdgesState,
-  ControlButton
+  ControlButton,
+  Panel
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-function CustomControls({ onNextNodeClick }) {
+
+
+
+function ViewPanel({ onNextNodeClick }) {
   return (
-    <Controls>
-      <ControlButton onClick={onNextNodeClick} title="action">
-        <div>D</div>
-      </ControlButton>
-    </Controls>
+
+    <Panel position="top-left"> <i className="fa-solid fa-eye fa-xl" onClick={onNextNodeClick} title="action"></i> </Panel>
   );
 }
 
@@ -23,18 +24,59 @@ const defaultViewport = { x: 90, y: 0, zoom: 0.6 };
 
 const pages_1 = [
   "https://i.ibb.co/StDvRJ4/01.jpg",
-  "https://i.ibb.co/tXMq7j0/02.jpg",
-  "https://i.ibb.co/zby6ScN/03.jpg",
+"https://i.ibb.co/tXMq7j0/02.jpg",
+"https://i.ibb.co/zby6ScN/03.jpg",
+"https://i.ibb.co/wQz6tbd/04.jpg",
+"https://i.ibb.co/Ldw2RDW/05.jpg",
+"https://i.ibb.co/n8JnD2g/06.jpg",
+"https://i.ibb.co/cCg4BjH/07.jpg",
+"https://i.ibb.co/PM97Rjh/08.jpg",
+"https://i.ibb.co/1L03CmM/09.jpg",
+"https://i.ibb.co/NF1qFD5/10.jpg",
+"https://i.ibb.co/YBt98ST/11.jpg",
+"https://i.ibb.co/dLwVn2L/12.jpg",
+"https://i.ibb.co/BPFKjY8/13.jpg",
+"https://i.ibb.co/CPHDzm1/14.jpg",
+"https://i.ibb.co/0GNZCZf/15.jpg",
+"https://i.ibb.co/LCJjW5x/16.jpg",
+"https://i.ibb.co/9GrSLKy/17.jpg",
+"https://i.ibb.co/6vjBMkS/18.jpg",
+"https://i.ibb.co/yWP4nry/19.jpg",
+"https://i.ibb.co/6wnHjZS/20.jpg",
+"https://i.ibb.co/bsfRTH5/21.jpg",
+"https://i.ibb.co/Nrcb5St/22.jpg",
+"https://i.ibb.co/GHYZfpY/23.jpg",
+"https://i.ibb.co/n75ykyy/24.jpg",
+"https://i.ibb.co/nc4tw7G/25.jpg",
+"https://i.ibb.co/JxdjD16/26.jpg",
+"https://i.ibb.co/h1CvNxD/27.jpg",
+"https://i.ibb.co/0GVRrZk/28.jpg",
+"https://i.ibb.co/gybFtrZ/29.jpg",
+"https://i.ibb.co/pb40tps/30.png",
+"https://i.ibb.co/pKZmNRb/31.png",
+"https://i.ibb.co/bRmqgy7/32.png",
+"https://i.ibb.co/X5vWZx1/33.png",
+"https://i.ibb.co/YkdtqPF/34.jpg",
+"https://i.ibb.co/XJC7yWp/35.jpg",
+"https://i.ibb.co/pWP0PzX/36.jpg",
+"https://i.ibb.co/KNBp0FC/37.jpg",
+"https://i.ibb.co/CHpbs4F/38.jpg",
+"https://i.ibb.co/bFJH3V0/39.jpg",
+"https://i.ibb.co/8xksDp7/40.jpg",
+"https://i.ibb.co/4SC68WX/41.jpg",
+"https://i.ibb.co/2Mqp4Wb/42.jpg",
+"https://i.ibb.co/pXr845X/43.jpg",
+"https://i.ibb.co/tKc985D/44.jpg",
+"https://i.ibb.co/7KLVVGT/45.jpg",
+"https://i.ibb.co/GdNk6Lg/46.jpg",
+"https://i.ibb.co/h2pwMNz/47.jpg",
+"https://i.ibb.co/z7HvLqn/48.jpg",
+"https://i.ibb.co/qyBh31j/49.jpg",
+"https://i.ibb.co/jzbSL4d/50.jpg"
 ];
 
 const stil = {
-  backgroundColor: '#edef',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundImage: 'url(https://i.ibb.co/jzbSL4d/50.jpg)',
   backgroundSize: 'cover',
-  backgroundPosition: 'center',
   width: 700,
   height: 980
 }
@@ -42,6 +84,7 @@ const stil = {
 const initialNodes = pages_1.map((page, index) => ({
   id: `${index + 1}`,
   position: { x: 0, y: index * 1050 },
+  draggaable: true,
   data: { label: `${index + 1}` },
   style: {
     ...stil,
@@ -55,11 +98,11 @@ const initialEdges = initialNodes.slice(0, initialNodes.length - 1).map((_, inde
   source: `${index + 1}`,
   target: `${index + 2}`,
   animated: true,
-  style: { stroke: 'red' },
+  style: { stroke: 'red', },
   label: 'Devam...',
   labelBgPadding: [10, 10],
   labelBgBorderRadius: 4,
-  labelBgStyle: { fill: '#FFCC00', color: '#fff' },
+  labelBgStyle: { fill: '#FFCC00', color: '#fff'  },
 }));
 
 export default function App() {
@@ -87,9 +130,11 @@ export default function App() {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={nodes} edges={edges} defaultViewport={defaultViewport} >
+      <ReactFlow nodes={nodes} edges={edges} defaultViewport={defaultViewport}  >
+      <Panel position="top-left"> <i className="fa-solid fa-eye fa-xl"></i> </Panel>
         <Background variant="dots" gap={12} size={1} />
-        <CustomControls onNextNodeClick={handleNextNodeClick} />
+        <Controls/>
+        <ViewPanel onNextNodeClick={handleNextNodeClick} />
       </ReactFlow>
     </div>
   );
